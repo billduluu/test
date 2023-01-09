7a41aaa46da24e4a9bb0d3ba63e1acb7536598a1
Duluu <Duluu@Duluus-MacBook-Air.local>
2023-01-09 10:10:59 +0800

    test2

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<div className={styles.description}>
 					<p>
-						Duluu124141414124124&nbsp;
+						s&nbsp;
 						<code className={styles.code}>pages/index.tsx</code>
 					</p>
 					<div>
